598bc72c7328a3ee09178c3427178ab7b78a8ced
kagemomiji <kagemomiji.dev@gmail.com>
2021-10-31 01:13:45 +0900

    [remove] purge ngrok and google tts

--- example.js
+++ example.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var googlehome = require('./google-home-notifier');
-var ngrok = require('ngrok');
 var bodyParser = require('body-parser');
 var app = express();
 const serverPort = 8091; // default port
@@ -15,41 +14,29 @@ app.post('/google-home-notifier', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
   console.log(req.body);
   
-  var text = req.body.text;
+  var file = req.body.file;
   
   if (req.query.ip) {
      ip = req.query.ip;
   }
 
-  var language = 'pl'; // default language code
-  if (req.query.language) {
-    language;
-  }
+  googlehome.ip(ip);
+  googlehome.device(deviceName);
 
-  googlehome.ip(ip, language);
-  googlehome.device(deviceName,language);
-
-  if (text){
+  if (file){
     try {
-      if (text.startsWith('http')){
-        var mp3_url = text;
-        googlehome.play(mp3_url, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
-        });
-      } else {
-        googlehome.notify(text, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will say: ' + text + '\n');
-        });
-      }
+      var mp3_url = file;
+      googlehome.play(mp3_url, function(notifyRes) {
+        console.log(notifyRes);
+        res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
+      });
     } catch(err) {
       console.log(err);
       res.sendStatus(500);
       res.send(err);
     }
   }else{
-    res.send('Please GET "text=Hello Google Home"');
+    res.send('Please GET "file=https%3A%2F%2Fexample.com%2Fhoge.mp3"');
   }
 })
 
@@ -57,52 +44,37 @@ app.get('/google-home-notifier', function (req, res) {
 
   console.log(req.query);
 
-  var text = req.query.text;
+  var file = req.query.file;
 
   if (req.query.ip) {
      ip = req.query.ip;
   }
 
-  var language = 'pl'; // default language code
-  if (req.query.language) {
-    language;
-  }
+  googlehome.ip(ip);
+  googlehome.device(deviceName);
 
-  googlehome.ip(ip, language);
-  googlehome.device(deviceName,language);
-
-  if (text) {
+  if (file) {
     try {
-      if (text.startsWith('http')){
-        var mp3_url = text;
-        googlehome.play(mp3_url, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
-        });
-      } else {
-        googlehome.notify(text, function(notifyRes) {
-          console.log(notifyRes);
-          res.send(deviceName + ' will say: ' + text + '\n');
-        });
-      }
+      var mp3_url = file;
+      googlehome.play(mp3_url, function(notifyRes) {
+        console.log(notifyRes);
+        res.send(deviceName + ' will play sound from url: ' + mp3_url + '\n');
+      });
     } catch(err) {
       console.log(err);
       res.sendStatus(500);
       res.send(err);
     }
   }else{
-    res.send('Please GET "text=Hello+Google+Home"');
+    res.send('Please GET "file=https%3A%2F%2Fexample.com%2Fhoge.mp3"');
   }
 })
 
 app.listen(serverPort, function () {
-  ngrok.connect(serverPort, function (err, url) {
-    console.log('Endpoints:');
-    console.log('    http://' + ip + ':' + serverPort + '/google-home-notifier');
-    console.log('    ' + url + '/google-home-notifier');
-    console.log('GET example:');
-    console.log('curl -X GET ' + url + '/google-home-notifier?text=Hello+Google+Home');
+  console.log('Endpoints:');
+  console.log('    http://' + ip + ':' + serverPort + '/google-home-notifier');
+  console.log('GET example:');
+  console.log('curl -X GET http://' + ip + ':' + serverPort + '/google-home-notifier?file=https%3A%2F%2Fexample.com%2Fhoge.mp3');
 	console.log('POST example:');
-	console.log('curl -X POST -d "text=Hello Google Home" ' + url + '/google-home-notifier');
-  });
+	console.log('curl -X POST -d "file=Hello Google Home" http://' + ip + ':' + serverPort + '/google-home-notifier');
 })
--- google-home-notifier.js
+++ google-home-notifier.js
@@ -3,46 +3,16 @@ var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
 var mdns = require('mdns');
 var browser = mdns.createBrowser(mdns.tcp('googlecast'));
 var deviceAddress;
-var language;
 
-var device = function(name, lang = 'en') {
+var device = function(name) {
     device = name;
-    language = lang;
     return this;
 };
 
-var ip = function(ip, lang = 'en') {
+var ip = function(ip) {
   deviceAddress = ip;
-  language = lang;
   return this;
 }
-
-var googletts = require('google-tts-api');
-var googlettsaccent = 'us';
-var accent = function(accent) {
-  googlettsaccent = accent;
-  return this;
-}
-
-var notify = function(message, callback) {
-  if (!deviceAddress){
-    browser.start();
-    browser.on('serviceUp', function(service) {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(device.replace(' ', '-'))){
-        deviceAddress = service.addresses[0];
-        getSpeechUrl(message, deviceAddress, function(res) {
-          callback(res);
-        });
-      }
-      browser.stop();
-    });
-  }else {
-    getSpeechUrl(message, deviceAddress, function(res) {
-      callback(res);
-    });
-  }
-};
 
 var play = function(mp3_url, callback) {
   if (!deviceAddress){
@@ -64,15 +34,6 @@ var play = function(mp3_url, callback) {
   }
 };
 
-var getSpeechUrl = function(text, host, callback) {
-  googletts(text, language, 1, 1000, googlettsaccent).then(function (url) {
-    onDeviceUp(host, url, function(res){
-      callback(res)
-    });
-  }).catch(function (err) {
-    console.error(err.stack);
-  });
-};
 
 var getPlayUrl = function(url, host, callback) {
     onDeviceUp(host, url, function(res){
@@ -106,6 +67,4 @@ var onDeviceUp = function(host, url, callback) {
 
 exports.ip = ip;
 exports.device = device;
-exports.accent = accent;
-exports.notify = notify;
 exports.play = play;
